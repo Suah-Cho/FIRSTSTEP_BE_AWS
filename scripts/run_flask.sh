5068cc34e4085ef8b83f47c449b8a77bc1e49479
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2023-11-24 16:02:25 +0900

    Update run_flask.sh

--- scripts/run_flask.sh
+++ scripts/run_flask.sh
@@ -4,5 +4,5 @@ cd  /home/ubuntu/firststep
 
 echo ">>> run app --------------------------"
 
-# python3 app.py > /dev/null 2> /dev/null < /dev/null &
-python3 app.py
+python3 app.py > /dev/null 2> /dev/null < /dev/null &
+# python3 app.py
